d5fdc662cb19a1cb6bba9603a208ba73a1c30e71
Freezystem <freezystem@gmail.com>
2022-07-13 14:19:53 +0200

    fix dependecy issue + deprecated use of metamask

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,31 +1,24 @@
-import Web3 from "web3/dist/web3.min.js";
+import Web3 from "web3";
 import Wallet from "./contracts/Wallet.json";
+import detectEthereumProvider from '@metamask/detect-provider';
 
-const getWeb3 = () => {
-  return new Promise((resolve, reject) => {
-    window.addEventListener("load", async () => {
-      if ( window.ethereum ) {
+const getWeb3 = () =>
+  new Promise( async (resolve, reject) => {
+    let provider = await detectEthereumProvider();
+    
+    if(provider) {
+      await provider.request({ method: 'eth_requestAccounts' });
+      try {
         const web3 = new Web3(window.ethereum);
-        try {
-          await web3.ethereum.enable();
-          resolve(web3);
-        }
-        catch ( error ) {
-          reject(error);
-        }
+        resolve(web3);
+      } catch(error) {
+        reject(error);
       }
-      else if ( window.web3 ) {
-        resolve(window.web3);
-      }
-      else {
-        reject("You MUST install MetaMask to use this App");
-      }
-    });
+    } reject('Install Metamask');
   });
-};
 
 const getWallet = async web3 => {
-  const networkId = await web3.eth.net.getId();
+  const networkId = await web3.eth.getChainId();
   const deployedNetwork = Wallet.networks[networkId];
 
   return new web3.eth.Contract(
